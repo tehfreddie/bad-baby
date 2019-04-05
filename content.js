1d55e0081633af3ad553a833d33d1f26ccbc7ec0
tehfreddie <49316725+tehfreddie@users.noreply.github.com>
2019-04-05 18:15:15 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,27 @@
-// content.js
-alert("Hello fellow bad babies!")
+/*jshint esversion: 6 */
+
+
+var elementsInsideBody = [...document.body.getElementsByTagName('*')];
+// This makes an array of everything inside the body tag
+
+
+//a function that loops through every single item
+function findAndReplace(){
+  elementsInsideBody.forEach(element =>{
+    element.childNodes.forEach(child =>{
+      if(child.nodeType === 3){
+        replaceText(child);
+      }
+    });
+
+  });
+}
+
+function replaceText (node) {
+  let value = node.nodeValue;
+  value = value.replace(/enfant terrible/gi, 'bad baby');
+  value = value.replace(/enfant terrible/gi, 'bad baby');
+  node.nodeValue = value;
+}
+
+window.onload = findAndReplace();
